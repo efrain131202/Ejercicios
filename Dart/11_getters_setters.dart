97f8e28dbb6762c491a4136641c4e0f74f39ec73
efrain131202 <efraincruzlobato13@gmail.com>
2025-02-21 13:11:45 -0600

    feat(11_getters_setters) New example

---
+++ Dart/11_getters_setters.dart
@@ -0,0 +1,32 @@
+void main() {
+  //Instancia de la clase
+  final miCuadrado = Cuadrado(lado: 10);
+
+  //se define valor de lado
+  miCuadrado.lado = 20;
+
+  //se imprime
+  print("Area: ${miCuadrado.area}");
+}
+
+//Clase Cuadrado
+class Cuadrado {
+  double _lado;
+
+  //Constructor
+  Cuadrado({required double lado}) : _lado = lado;
+
+  //Getters
+  double get area {
+    return _lado * _lado;
+  }
+
+  //Setters
+  set lado(double value) {
+    print("new value $value");
+    if (value <= 0) {
+      throw ("El lado no puede ser menor o igual a 0");
+    }
+    _lado = value;
+  }
+}
